6f1736650b766f3f633064cd3e0c665088ab9773
AakeJon49 <38993117+AakeJon49@users.noreply.github.com>
2022-10-30 21:29:45 +0100

    Uppdatera pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-input.onButtonPressed(Button.A, function () {
-    for (let plats = 0; plats <= 2; plats++) {
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-        basic.showNumber(3 - plats)
+let baslinje = input.magneticForce(Dimension.Strength)
+basic.forever(function () {
+    if (Math.abs(input.magneticForce(Dimension.Strength) - baslinje) > 100) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.clearScreen()
     }
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    basic.showString("Go")
 })
